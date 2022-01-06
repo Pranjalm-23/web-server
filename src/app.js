7c31f112e34419cdd5593f1a5cf0eab4ee66dc73
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-01-06 22:28:54 +0530

    geocode and forecast utils added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
 
 const app = express();
 
@@ -54,11 +58,29 @@ app.get('/weather', (req, res) => {
       })
   }
 
-  res.send({
-      forecast: 'It is snowing',
-      location: 'Philadelphia',
-      address: req.query.address
+//   res.send({
+//       forecast: 'It is snowing',
+//       location: 'Bhopal',
+//       address: req.query.address
+//   })
+// })
+geocode(req.query.address, (error, { latitude, longitude, location }) => {
+  if (error) {
+      return res.send({ error })
+  }
+
+  forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+          return res.send({ error })
+      }
+
+      res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address
+      })
   })
+})
 })
 
 app.get('/products', (req, res) => {
