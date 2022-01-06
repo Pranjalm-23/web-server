9f02598b8c4872eabaa5e5bbf49319c26de21f5b
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-01-06 23:33:41 +0530

    App version 1.0.0 release ready

--- src/app.js
+++ src/app.js
@@ -41,8 +41,6 @@ app.get("/help", (req, res) => {
   });
 });
 
-
-
 app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({
@@ -50,23 +48,26 @@ app.get("/weather", (req, res) => {
     });
   }
 
-  geocode(req.query.address, (error, { latitude, longitude, location }={}) => {
-    if (error) {
-      return res.send({ error });
-    }
-
-    forecast(latitude, longitude, (error, forecastData) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error });
       }
 
-      res.send({
-        forecast: forecastData,
-        location,
-        address: req.query.address,
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get("/products", (req, res) => {
