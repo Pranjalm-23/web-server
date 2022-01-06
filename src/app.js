6eddb73e0ddcfa6b6403f4ebf15fdc4f40215e6a
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-01-06 22:37:07 +0530

    default parameter set

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
-
-
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -51,50 +49,50 @@ app.get("/help", (req, res) => {
 //   });
 // });
 
-app.get('/weather', (req, res) => {
+app.get("/weather", (req, res) => {
   if (!req.query.address) {
-      return res.send({
-          error: 'You must provide an address!'
-      })
+    return res.send({
+      error: "You must provide an address!",
+    });
   }
 
-//   res.send({
-//       forecast: 'It is snowing',
-//       location: 'Bhopal',
-//       address: req.query.address
-//   })
-// })
-geocode(req.query.address, (error, { latitude, longitude, location }) => {
-  if (error) {
-      return res.send({ error })
-  }
+  //   res.send({
+  //       forecast: 'It is snowing',
+  //       location: 'Bhopal',
+  //       address: req.query.address
+  //   })
+  // })
+  geocode(req.query.address, (error, { latitude, longitude, location }={}) => {
+    if (error) {
+      return res.send({ error });
+    }
 
-  forecast(latitude, longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
-          return res.send({ error })
+        return res.send({ error });
       }
 
       res.send({
-          forecast: forecastData,
-          location,
-          address: req.query.address
-      })
-  })
-})
-})
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
+});
 
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   if (!req.query.search) {
-      return res.send({
-          error: 'You must provide a search term'
-      })
+    return res.send({
+      error: "You must provide a search term",
+    });
   }
 
-  console.log(req.query.search)
+  console.log(req.query.search);
   res.send({
-      products: []
-  })
-})
+    products: [],
+  });
+});
 
 app.get("/help/*", (req, res) => {
   res.render("404", {
