7be8eab6e9edb37c828adcecf4158a8338e56caf
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-01-06 23:11:03 +0530

    form added and handled

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,34 @@
-console.log('Client side javascript file is loaded!')
+console.log("Client side javascript file is loaded!");
 
-console.log('Client side javascript file is loaded 2!')
+// console.log("Client side javascript file is loaded 2!");
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+// fetch("http://localhost:3000/weather?address=rewa").then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log(data.error);
+//     } else {
+//       console.log(data.location);
+//       console.log(data.forecast);
+//     }
+//   });
+// });
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
